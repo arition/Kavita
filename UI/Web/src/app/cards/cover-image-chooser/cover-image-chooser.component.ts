02394d3f0fb941cb6697b9091e3d6dc591b4d22c
arition <aritionkb@gmail.com>
2025-01-25 02:32:11 -0800

    feat: add support for bmp and tif images

--- UI/Web/src/app/cards/cover-image-chooser/cover-image-chooser.component.ts
+++ UI/Web/src/app/cards/cover-image-chooser/cover-image-chooser.component.ts
@@ -82,7 +82,7 @@ export class CoverImageChooserComponent implements OnInit {
   appliedIndex: number = 0;
   form!: FormGroup;
   files: NgxFileDropEntry[] = [];
-  acceptableExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.webp', '.avif'].join(',');
+  acceptableExtensions = ['.png', '.jpg', '.jpeg', '.gif', '.webp', '.avif', '.bmp', '.tif'].join(',');
   mode: 'file' | 'url' | 'all' = 'all';
 
   constructor(@Inject(DOCUMENT) private document: Document) { }
